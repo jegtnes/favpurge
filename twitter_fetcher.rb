c07a6cbb05fd7c05bbcfb87624187d11b620564d
Alex Jegtnes <alex@jegtnes.co.uk>
2014-06-07 11:00:12 +0100

    pleasing rubocop

--- twitter_fetcher.rb
+++ twitter_fetcher.rb
@@ -3,18 +3,18 @@ require 'sinatra/jsonp'
 require 'rubygems'
 require 'twitter'
 
+# Fetch tweets, yo.
 class TwitterFetcher < Sinatra::Base
   helpers Sinatra::Jsonp
 
-  @@twitter_client = Twitter::Client.new(
-    :consumer_key       => 'YOUR-CONSUMER-KEY-JUNX',
-    :consumer_secret    => 'YOUR-CONSUMER-SECRET-JUNX',
-    :oauth_token        => 'YOUR-OAUTH-TOKEN-JUNX',
-    :oauth_token_secret => 'YOUR-OAUTH-TOKEN-SECRET-JUNX',
+  @twitter_client = Twitter::Client.new(
+    consumer_key:       'YOUR-CONSUMER-KEY-JUNX',
+    consumer_secret:    'YOUR-CONSUMER-SECRET-JUNX',
+    oauth_token:        'YOUR-OAUTH-TOKEN-JUNX',
+    oauth_token_secret: 'YOUR-OAUTH-TOKEN-SECRET-JUNX'
   )
 
   get '/' do
     jsonp @@twitter_client.home_timeline.map(&:attrs)
   end
-
 end
