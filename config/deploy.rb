5db5eb4d89a8e728b4fdb3ab81ba1d58ea0a4bb1
Alex Jegtnes <alex@jegtnes.co.uk>
2014-06-12 15:08:31 +0100

    Cleaned up Cap config

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,29 +16,8 @@ set :tmp_dir, "#{fetch(:host_root)}/tmp"
 set :user, 'ajms'
 set :scm_username, 'jegtnes'
 set :use_sudo, false
-# Default value for :scm is :git
-# set :scm, :git
 
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
 set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
 
 namespace :deploy do
 
@@ -51,13 +30,5 @@ namespace :deploy do
 
   after :publishing, :restart
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
 
 end
