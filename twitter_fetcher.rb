a09a46a4b0c0063830070544a8bdb42913db2967
Alex Jegtnes <alex@jegtnes.co.uk>
2014-11-08 22:37:35 +0000

    Fixed incorrect variable name assigned and commited by accident

--- twitter_fetcher.rb
+++ twitter_fetcher.rb
@@ -33,7 +33,7 @@ class TwitterFetcher < Sinatra::Base
 
   get '/' do
     if logged_in?
-      @client = Twitter::REST::Client.new do |config|
+      client = Twitter::REST::Client.new do |config|
         config.consumer_key        = ENV['CONSUMER_KEY']
         config.consumer_secret     = ENV['CONSUMER_SECRET']
         config.access_token        = session[:oauth_token]
